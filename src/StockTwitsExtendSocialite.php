b944b91784a05a88445ee38a98516ed6d3ffd0dd
DraperStudio <hello@draperstud.io>
2015-03-07 12:01:30 +0100

    Update comments

--- src/StockTwitsExtendSocialite.php
+++ src/StockTwitsExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class StockTwitsExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
